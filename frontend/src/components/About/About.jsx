4c8e28050dc2b80c1c95f0529ddf1db5423e503f
PRAVEEN KUMAR <115071711+Praveenkumar07007@users.noreply.github.com>
2024-03-23 09:25:14 +0530

    added content in achievement sections

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -14,7 +14,7 @@ const About = () => {
             We believe in the transformative power of staying connected and
             supporting one another. Our platform is not just about technology;
             it's about the collective spirit and energy of our alumni community.
-            Meet our team [Link to a team member page, if you have one], and
+            Meet our team , and
             join us in shaping the future of our alumni network.
           </p>
         </div>
@@ -33,14 +33,14 @@ const About = () => {
             Our Story
           </h1>
           <p className="font-normal text-base leading-6 text-gray-600 ">
-            "At aluminilinkup, we understand that graduating is merely the
+            At aluminilinkup, we understand that graduating is merely the
             beginning of your journey. Our goal is to facilitate your continued
             growth, both personally and professionally. Our platform offers a
             wealth of resources, from mentorship programs and career
             opportunities to exclusive events and industry insights. We believe
             that the shared experiences and expertise within our alumni network
             have the potential to ignite your ambitions. Join aluminilinkup and
-            discover a community dedicated to your success."
+            discover a community dedicated to your success.
           </p>
         </div>
         <div className="w-full lg:w-8/12 lg:pt-8">
--- frontend/src/components/Achievements/Achievements.jsx
+++ frontend/src/components/Achievements/Achievements.jsx
@@ -7,7 +7,7 @@ const Achievements = () => {
       id: 1,
       name: "Alumini Friends",
       description:
-        "Ms. Priya Singh's project featured as 'Project of the Week' on a popular open-source platform.",
+        "Agriculture Student From Lovely Professional University Bags A Rs. 1 Crore Package",
 
       imageSrc:
         "https://images.unsplash.com/photo-1504600770771-fb03a6961d33?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8c3F1YXJlfGVufDB8fDB8fHww",
@@ -17,7 +17,7 @@ const Achievements = () => {
       id: 2,
       name: "Alumini Friends",
       description:
-        "Mr. Rahul Gupta surpassed 1,000 contributions to major open-source repositories.",
+        "LPU B.Tech. CSE student Mayank Bist has been placed at a grand package of Rs. 50.53 Lakhs.",
 
       imageSrc:
         "https://images.unsplash.com/photo-1552664730-d307ca884978?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fFByb2plY3R8ZW58MHx8MHx8fDA%3D",
@@ -27,17 +27,17 @@ const Achievements = () => {
       id: 3,
       name: "Alumini Friends",
       description:
-        "10 AlumniLinkUp members received prestigious tech scholarships through our networking initiatives.",
+        "LPU B.Tech. CSE student Tanya Arora has been placed at a grand package of Rs. 42 Lakhs.",
 
       imageSrc:
-        "https://plus.unsplash.com/premium_photo-1661766292640-abf05d03a1ac?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fFByb2plY3R8ZW58MHx8MHx8fDA%3D",
+        "https://qph.cf2.quoracdn.net/main-qimg-3010f7c667aee922e00e1f10a141859e-lq",
       imageAlt: "Alumini",
     },
     {
       id: 4,
       name: "Alumini Friends",
       description:
-        "Ms. Neha Patel selected as keynote speaker at a national industry conference.",
+        "LPU student Neeraj Chopra made historic win at World Athletics Championships-2022 in U.S.",
 
       imageSrc:
         "https://media.istockphoto.com/id/855256042/photo/engineers-of-wind-turbine.webp?b=1&s=170667a&w=0&k=20&c=7k4p_03udn5SDWUtdk9Kyv5PrL6umEPZWKHKh_K8lvg=",
@@ -47,7 +47,7 @@ const Achievements = () => {
       id: 5,
       name: "Alumini Friends",
       description:
-        "AlumniLinkUp mentorship program led to a 20% increase in alumni career advancements.",
+        "AUtkarsh, a wireless system engineer working with Google, California, and Neha, a software engineer with Yahoo also express their gratitude towards LPU in shaping the careers of their dreams.",
 
       imageSrc:
         "https://media.istockphoto.com/id/1903901827/photo/view-of-compasses-pencil-drawing-background.webp?b=1&s=170667a&w=0&k=20&c=c494W9u5NOKJXGjd62FfToi7a2XG6cqva12QIV8TrC0=",
